775ee732055ac4cd204d633118fbe5dfabb56fc0
Abdulloh <developer15pro@gmail.com>
2022-02-07 02:52:11 +0500

    finish driver

--- service/driver.go
+++ service/driver.go
@@ -1,20 +1,58 @@
 package service
 
 import (
+	"context"
+
+	pb "github.com/abdullohsattorov/mytaxi-service/genproto"
 	l "github.com/abdullohsattorov/mytaxi-service/pkg/logger"
-	"github.com/abdullohsattorov/mytaxi-service/storage"
+	"github.com/gofrs/uuid"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
-// TaxiService is an object that implements TaxiServiceServer interface in genproto.
-type DriverService struct {
-	storage storage.IStorage
-	logger  l.Logger
+func (t *MyTaxiService) CreateDriver(ctx context.Context, req *pb.Driver) (*pb.Driver, error) {
+	id, err := uuid.NewV4()
+	if err != nil {
+		t.logger.Error("failed while generating uuid", l.Error(err))
+		return nil, status.Error(codes.Internal, "failed generate uuid")
+	}
+	req.Id = id.String()
+
+	driver, err := t.storage.Driver().CreateDriver(*req)
+	if err != nil {
+		t.logger.Error("failed to create order", l.Error(err))
+		return nil, status.Error(codes.Internal, "failed to create order")
+	}
+
+	return &driver, nil
 }
 
-// NewTaxiService ...
-func NewDriverService(storage storage.IStorage, log l.Logger) *DriverService {
-	return &DriverService{
-		storage: storage,
-		logger:  log,
+func (t *MyTaxiService) GetDriver(ctx context.Context, req *pb.ByIdReq) (*pb.Driver, error) {
+	driver, err := t.storage.Driver().GetDriver(req.GetId())
+	if err != nil {
+		t.logger.Error("failed to get task", l.Error(err))
+		return nil, status.Error(codes.Internal, "failed to get order")
 	}
+
+	return &driver, nil
+}
+
+func (t *MyTaxiService) UpdateDriver(ctx context.Context, req *pb.Driver) (*pb.Driver, error) {
+	driver, err := t.storage.Driver().UpdateDriver(*req)
+	if err != nil {
+		t.logger.Error("failed to update driver", l.Error(err))
+		return nil, status.Error(codes.Internal, "failed to update order")
+	}
+
+	return &driver, nil
+}
+
+func (t *MyTaxiService) DeleteDriver(ctx context.Context, req *pb.ByIdReq) (*pb.EmptyResp, error) {
+	err := t.storage.Driver().DeleteDriver(req.Id)
+	if err != nil {
+		t.logger.Error("failed to delete driver", l.Error(err))
+		return nil, status.Error(codes.Internal, "failed to delete order")
+	}
+
+	return &pb.EmptyResp{}, nil
 }
--- storage/postgres/driver.go
+++ storage/postgres/driver.go
@@ -1,7 +1,12 @@
 package postgres
 
 import (
+	"database/sql"
+	"time"
+
 	"github.com/jmoiron/sqlx"
+
+	pb "github.com/abdullohsattorov/mytaxi-service/genproto"
 )
 
 type driverRepo struct {
@@ -11,4 +16,73 @@ type driverRepo struct {
 // NewTaxiRepo ...
 func NewDriverRepo(db *sqlx.DB) *driverRepo {
 	return &driverRepo{db: db}
+}
+
+func (t *driverRepo) CreateDriver(driver pb.Driver) (pb.Driver, error) {
+	var id string
+	err := t.db.QueryRow(`
+        INSERT INTO drivers(id, first_name, last_name, phone, car_model, created_at, updated_at)
+        VALUES ($1, $2, $3, $4, $5, $6, $7) returning id`, driver.Id, driver.FirstName, driver.LastName, driver.Phone, driver.CarModel, time.Now().UTC(), time.Now().UTC()).Scan(&id)
+	if err != nil {
+		return pb.Driver{}, err
+	}
+	driver, err = t.GetDriver(id)
+
+	if err != nil {
+		return pb.Driver{}, err
+	}
+
+	return driver, nil
+}
+
+func (t *driverRepo) GetDriver(id string) (pb.Driver, error) {
+	var driver pb.Driver
+
+	err := t.db.QueryRow(`
+        SELECT id, first_name, last_name, phone, car_model, created_at, updated_at FROM drivers
+        WHERE id=$1 and deleted_at is null`, id).Scan(
+		&driver.Id,
+		&driver.FirstName,
+		&driver.LastName,
+		&driver.Phone,
+		&driver.CarModel,
+		&driver.CreatedAt,
+		&driver.UpdatedAt,
+	)
+	if err != nil {
+		return pb.Driver{}, err
+	}
+	return driver, nil
+}
+
+func (t *driverRepo) UpdateDriver(driver pb.Driver) (pb.Driver, error) {
+	result, err := t.db.Exec(`UPDATE drivers SET first_name=$2, last_name=$3, phone=$4, car_model=$5, updated_at=$6 WHERE id=$1`,
+		driver.Id, driver.FirstName, driver.LastName, driver.Phone, driver.CarModel, time.Now().UTC())
+	if err != nil {
+		return pb.Driver{}, err
+	}
+
+	if i, _ := result.RowsAffected(); i == 0 {
+		return pb.Driver{}, sql.ErrNoRows
+	}
+
+	driver, err = t.GetDriver(driver.Id)
+	if err != nil {
+		return pb.Driver{}, err
+	}
+
+	return driver, nil
+}
+
+func (t *driverRepo) DeleteDriver(id string) error {
+	result, err := t.db.Exec(`UPDATE drivers SET deleted_at = $2 WHERE id=$1`, id, time.Now().UTC())
+	if err != nil {
+		return err
+	}
+
+	if i, _ := result.RowsAffected(); i == 0 {
+		return sql.ErrNoRows
+	}
+
+	return nil
 }
