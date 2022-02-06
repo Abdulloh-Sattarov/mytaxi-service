f3660903780c7d25a60c89192cd0ff3d05b33016
Abdulloh <developer15pro@gmail.com>
2022-02-07 02:37:32 +0500

    finish client

--- service/client.go
+++ service/client.go
@@ -3,27 +3,14 @@ package service
 import (
 	"context"
 
+	pb "github.com/abdullohsattorov/mytaxi-service/genproto"
 	l "github.com/abdullohsattorov/mytaxi-service/pkg/logger"
-	"github.com/abdullohsattorov/mytaxi-service/storage"
+	"github.com/gofrs/uuid"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
-// TaxiService is an object that implements TaxiServiceServer interface in genproto.
-type ClientService struct {
-	storage storage.IStorage
-	logger  l.Logger
-}
-
-// NewTaxiService ...
-func NewClientService(storage storage.IStorage, log l.Logger) *ClientService {
-	return &ClientService{
-		storage: storage,
-		logger:  log,
-	}
-}
-
-func (t *ClientService) CreateClient(ctx context.Context, req *pb.Client) (*pb.Client, error) {
+func (t *MyTaxiService) CreateClient(ctx context.Context, req *pb.Client) (*pb.Client, error) {
 	id, err := uuid.NewV4()
 	if err != nil {
 		t.logger.Error("failed while generating uuid", l.Error(err))
@@ -31,7 +18,7 @@ func (t *ClientService) CreateClient(ctx context.Context, req *pb.Client) (*pb.C
 	}
 	req.Id = id.String()
 
-	client, err := t.storage.Taxi().CreateClient(*req)
+	client, err := t.storage.Client().CreateClient(*req)
 	if err != nil {
 		t.logger.Error("failed to create client", l.Error(err))
 		return nil, status.Error(codes.Internal, "failed to create order")
@@ -40,8 +27,8 @@ func (t *ClientService) CreateClient(ctx context.Context, req *pb.Client) (*pb.C
 	return &client, nil
 }
 
-func (t *ClientService) GetClient(ctx context.Context, req *pb.ByIdReq) (*pb.Client, error) {
-	client, err := t.storage.Taxi().GetClient(req.GetId())
+func (t *MyTaxiService) GetClient(ctx context.Context, req *pb.ByIdReq) (*pb.Client, error) {
+	client, err := t.storage.Client().GetClient(req.GetId())
 	if err != nil {
 		t.logger.Error("failed to get client", l.Error(err))
 		return nil, status.Error(codes.Internal, "failed to get order")
@@ -50,8 +37,8 @@ func (t *ClientService) GetClient(ctx context.Context, req *pb.ByIdReq) (*pb.Cli
 	return &client, nil
 }
 
-func (t *ClientService) UpdateClient(ctx context.Context, req *pb.Client) (*pb.Client, error) {
-	client, err := t.storage.Taxi().UpdateClient(*req)
+func (t *MyTaxiService) UpdateClient(ctx context.Context, req *pb.Client) (*pb.Client, error) {
+	client, err := t.storage.Client().UpdateClient(*req)
 	if err != nil {
 		t.logger.Error("failed to update client", l.Error(err))
 		return nil, status.Error(codes.Internal, "failed to update order")
@@ -60,8 +47,8 @@ func (t *ClientService) UpdateClient(ctx context.Context, req *pb.Client) (*pb.C
 	return &client, nil
 }
 
-func (t *ClientService) DeleteClient(ctx context.Context, req *pb.ByIdReq) (*pb.EmptyResp, error) {
-	err := t.storage.Taxi().DeleteClient(req.Id)
+func (t *MyTaxiService) DeleteClient(ctx context.Context, req *pb.ByIdReq) (*pb.EmptyResp, error) {
+	err := t.storage.Client().DeleteClient(req.Id)
 	if err != nil {
 		t.logger.Error("failed to delete client", l.Error(err))
 		return nil, status.Error(codes.Internal, "failed to delete order")
--- storage/postgres/client.go
+++ storage/postgres/client.go
@@ -1,7 +1,12 @@
 package postgres
 
 import (
+	"database/sql"
+	"time"
+
 	"github.com/jmoiron/sqlx"
+
+	pb "github.com/abdullohsattorov/mytaxi-service/genproto"
 )
 
 type clientRepo struct {
@@ -11,4 +16,72 @@ type clientRepo struct {
 // NewTaxiRepo ...
 func NewClientRepo(db *sqlx.DB) *clientRepo {
 	return &clientRepo{db: db}
+}
+
+func (t *clientRepo) CreateClient(client pb.Client) (pb.Client, error) {
+	var id string
+	err := t.db.QueryRow(`
+        INSERT INTO clients(id, first_name, last_name, phone, created_at, updated_at)
+        VALUES ($1, $2, $3, $4, $5, $6) returning id`, client.Id, client.FirstName, client.LastName, client.Phone, time.Now().UTC(), time.Now().UTC()).Scan(&id)
+	if err != nil {
+		return pb.Client{}, err
+	}
+	client, err = t.GetClient(id)
+
+	if err != nil {
+		return pb.Client{}, err
+	}
+
+	return client, nil
+}
+
+func (t *clientRepo) GetClient(id string) (pb.Client, error) {
+	var client pb.Client
+
+	err := t.db.QueryRow(`
+        SELECT id, first_name, last_name, phone, created_at, updated_at FROM clients
+        WHERE id=$1 and deleted_at is null`, id).Scan(
+		&client.Id,
+		&client.FirstName,
+		&client.LastName,
+		&client.Phone,
+		&client.CreatedAt,
+		&client.UpdatedAt,
+	)
+	if err != nil {
+		return pb.Client{}, err
+	}
+	return client, nil
+}
+
+func (t *clientRepo) UpdateClient(client pb.Client) (pb.Client, error) {
+	result, err := t.db.Exec(`UPDATE clients SET first_name=$2, last_name=$3, phone=$4, updated_at = $5 WHERE id=$1`,
+		client.Id, client.FirstName, client.LastName, client.Phone, time.Now().UTC())
+	if err != nil {
+		return pb.Client{}, err
+	}
+
+	if i, _ := result.RowsAffected(); i == 0 {
+		return pb.Client{}, sql.ErrNoRows
+	}
+
+	res, err := t.GetClient(client.Id)
+	if err != nil {
+		return pb.Client{}, err
+	}
+
+	return res, nil
+}
+
+func (t *clientRepo) DeleteClient(id string) error {
+	result, err := t.db.Exec(`UPDATE clients SET deleted_at = $2 WHERE id=$1`, id, time.Now().UTC())
+	if err != nil {
+		return err
+	}
+
+	if i, _ := result.RowsAffected(); i == 0 {
+		return sql.ErrNoRows
+	}
+
+	return nil
 }
